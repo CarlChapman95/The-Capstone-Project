2a3a91066554f37540461c7160bc03eaa8c345cc
CarlChapman95 <crabb.chap@gmail.com>
2025-07-28 21:17:05 -0400

    setup server and hidden api place

--- server.js
+++ server.js
@@ -6,7 +6,20 @@ const port = 8080;
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use("/api/v1/getRandomExersice", (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: process.env.CLIENT_ID
+    });
+});
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
 })
+
+require("dotenv").config();
+const cors = require("cors");
+const corsOptions = {
+    origin: `http://localhost:${port}`
+}
+app.use(cors());
